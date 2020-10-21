a8cad3c5e6e6d0e65d19e2baf2685cfdb4535e2f
Felipe Schauren <felipe.schauren@gmail.com>
2020-10-21 23:49:18 +0900

    initial rascal page design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 AOS.init();
+
+const uchiwa = document.getElementById("rascal-uchiwa");
+
+function flipUchiwa() {
+  const flipping = document.querySelector(".flip-card");
+  flipping.classList.toggle("flipping");
+}
+
+uchiwa.addEventListener("click", flipUchiwa);
